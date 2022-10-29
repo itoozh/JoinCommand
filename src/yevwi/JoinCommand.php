de5b43267c82ba1d42240de07942782aa05cccf9
yevwi <109642189+yevwi@users.noreply.github.com>
2022-10-29 16:38:27 -0500

    Update JoinCommand.php

--- src/yevwi/JoinCommand.php
+++ src/yevwi/JoinCommand.php
@@ -47,14 +47,4 @@ class JoinCommand  extends PluginBase implements Listener
         }
     }
 
-    public function onJoin(PlayerJoinEvent $ev): void
-    {
-        if(($p = $ev->getPlayer())->hasPlayedBefore()) return;
-        $nick = "\"{$p->getName()}\"";
-        $commands = $this->getConfig()->get("commands");
-        foreach ($commands as $command) {
-            Server::getInstance()->dispatchCommand(new ConsoleCommandSender($server = Server::getInstance(), $server->getLanguage()), str_replace("{player}",  $nick, $command));
-        }
-    }
-
 }
